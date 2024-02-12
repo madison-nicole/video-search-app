99f304c4718b668fbb1b0dae6c519d8db89a5abc
Madison Saylor <maddie0264@gmail.com>
2024-02-11 23:54:35 -0500

    added routing support

--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
-import $ from 'jquery';
-import './style.scss';
+import React from 'react';
+import {
+  BrowserRouter, Routes, Route, NavLink, useParams,
+} from 'react-router-dom';
 
-let num = 0;
+function App(props) {
+  return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+          <Route element={<Welcome />} path="/" />
+          <Route element={<About />} path="/about" />
+          <Route element={<Test />} path="/test/:id" />
+          <Route element={<FallBack />} path="*" />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
 
-setInterval(() => {
-  $('#main').html(`You've been on this page for ${num} seconds.`);
-  num += 1;
-}, 1000);
+function FallBack(props) {
+  return <div>URL Not Found</div>;
+}
+
+function Nav(props) {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+
+      </ul>
+    </nav>
+  );
+}
+
+function Test(props) {
+  const { id } = useParams();
+  return <div> ID: {id} </div>;
+}
+
+function About(props) {
+  return <div> All there is to know about me </div>;
+}
+function Welcome(props) {
+  return <div>Welcome</div>;
+}
